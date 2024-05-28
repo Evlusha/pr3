9b055f4365f6d1ecc43be0f19a432e36ebfaec6c
Evlusha <106895767+Evlusha@users.noreply.github.com>
2024-05-28 16:08:15 +0800

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,5 +1,5 @@
-/*Вова решил посмеяться над инфобезом и дал им текст с реверсироваными словами 
-Необходимо реверсировать слова в строке не изменяя порядок слов. */
+/*Вова решил посмеяться над инфобезом и дал им загадку с реверсироваными словами
+Необходимо реверсировать слова в строке не изменяя порядок слов и ответить на загадку */
 
 #include <iostream>
 #include <algorithm>
@@ -10,15 +10,27 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    string sentence = "Annushka has already bought sunflower oil, and not only bought it, but even spilled it";
+    string sentence = "атЭ ьщев ончыбо тисев в мотсуп екиньлидолох и юе язьлен ясьтавозьлоп ан 1 екиткарп ";
     istringstream iss(sentence);
     string word;
     string result;
-
+    string vopros = "Мышь";
     while (iss >> word) {
         reverse(word.begin(), word.end());
         result += word + " ";
     }
+   
+    cout << "\nРеверсированные слова:\n " << result;
+    cout << "\nВашь ответ ? \n" ;
+    string otv;
+    cin >> otv;
+    if (vopros == otv)
+    {
+        cout << "Вы гений ";
+    }
+    else
+    {
+        cout << " Ты пытался (";
+    }
 
-    cout << "Реверсированные слова: " << result;
 }
